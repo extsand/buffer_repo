6a6e62f697ff57f7c236fa005d66cca7a7cae5f8
Alexander <extsand@gmail.com>
2021-10-14 22:34:13 +0300

    Update java_input_console.java

--- java_input_console.java
+++ java_input_console.java
@@ -1,50 +1,59 @@
+package com.extsand;
 import java.util.Scanner;
 
-public class Program {
-   
-    public static void main(String[] args) {
-        
-        System.out.println("Hello Evgeniya");
-				// Это конструктор для ввода из консоли
-				Scanner in = new Scanner(System.in);
-				// Переменная для ответа
-				int answer = 0;
+public class Main {
 
-				// Обрабатываем переменную 1
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello Evgeniya");
+        // Это конструктор для ввода из консоли
+        Scanner in = new Scanner(System.in);
+        // Переменная для ответа
+        int answer;
+
+        // Обрабатываем переменную 1
         System.out.print("Input a value 1: ");
         int value_1 = in.nextInt();
 
-				// Обрабатываем оператор
-				System.out.println("Input operator ");
-				char operator =  in.nextLine();
+        // Обрабатываем оператор
+        System.out.println("Input operator ");
+        char operator = in.next().charAt(0);
 
-				// Обрабатываем переменную 2
-				System.out.print("Input a value 1: ");
+        // Обрабатываем переменную 2
+        System.out.print("Input a value 2: ");
         int value_2 = in.nextInt();
 
-				switch (operator) {
-					case  ('+'):
-						// Выполняем сложение и выводим это на экран
-						answer = value_1 + value_2
-						System.out.println("Your answer is: ", answer);
-						break;
-					case  ('-'):
-						// Выполняем вычитание и выводим это на экран
-						break;
-					case  ('/'):
-						// Выполняем 
-						break;
-					case  ('*'):
-						// Выполняем 
-						break;
-           
-					default:
-              // А если мы ничего не ввели?
-							System.out.print("Youre operator is wrong. Try again");
-						break;
-       }
+        switch (operator) {
+            case  ('+'):
+                // Выполняем сложение и выводим это на экран
+                answer = value_1 + value_2;
+                System.out.println("Your answer is: " + answer);
+                break;
+            case  ('-'):
+                // Выполняем вычитание и выводим это на экран
+                answer = value_1 - value_2;
+                System.out.println("Your answer is: " + answer);
+                break;
+            case  ('/'):
+                if (value_2 == 0){
+                    System.out.println("We can't divide in 0");
+                } else {
+                    answer = value_1 / value_2;
+                    System.out.println("Your answer is: " + answer);
+                }
 
 
+                break;
+            case  ('*'):
+                answer = value_1 * value_2;
+                System.out.println("Your answer is: " + answer);
+                break;
+
+            default:
+                // А если мы ничего не ввели?
+                System.out.print("Youre operator is wrong. Try again");
+                break;
+        }
 
 
     }
